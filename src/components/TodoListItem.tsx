6e2465452718fdaf3e0b9ede3457db5c0165c7cd
Leonardo Buck <leonardo@timais.com>
2021-10-17 20:01:04 -0300

    Manipulando eventos

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -6,11 +6,16 @@ interface TodoListItemProps {
 }
 
 const TodoListItem = (props: TodoListItemProps) => {
+    const handleChange = (event: any) => {
+        console.log('Mudou');
+    }
+
     return (
         <tr className="uk-animation-slide-bottom-medium">
             <td className="uk-width-auto">
                 <label>
-                    <input className="uk-checkbox" type="checkbox" />
+                    <input className="uk-checkbox" type="checkbox" checked={props.todo.done} 
+                        onChange={handleChange} />
                 </label>
             </td>
             <td className="uk-width-expand">{props.todo.title}</td>
