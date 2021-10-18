14afa2399cba26c0a7755826de9bc898e484c5cc
Leonardo Buck <leonardo@timais.com>
2021-10-18 13:24:39 -0300

    React Router

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTodo = () => {
+    return (<h1>Novo todo</h1>)
+};
+
+export default AddTodo;
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TodoContext from '../contexts/TodoContext';
+import AddTodo from './AddTodo';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
 
 const App = () => {
     return (
         <TodoContext>
-            <div className="uk-container">
+            <Router>
                 <Navbar></Navbar>
-                <TodoList></TodoList>
-            </div>
+                <br />
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </TodoContext>
     );
 }
